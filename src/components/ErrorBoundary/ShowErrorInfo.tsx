86a9f4f539ad0e2d2f4eff9bc70ffd6d6122788c
pinkpony1106 <juliaegorova221098@gmail.com>
2023-11-09 10:56:29 +0300

    fix: deploy, change class to fc

--- src/components/ErrorBoundary/ShowErrorInfo.tsx
+++ src/components/ErrorBoundary/ShowErrorInfo.tsx
@@ -1,27 +1,18 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import styles from './index.module.css';
 
-class ShowErrorInfo extends Component<
-  Record<string, never>,
-  { showError: boolean }
-> {
-  constructor(props: Record<string, never>) {
-    super(props);
-    this.state = { showError: false };
+function ShowErrorInfo() {
+  const [showErrorInfo, setShowErrorInfo] = useState<boolean>();
+
+  if (showErrorInfo) {
+    throw new Error('Error!');
   }
-  showError = () => {
-    this.setState({ showError: true });
-  };
-  render() {
-    if (this.state.showError) {
-      throw new Error('Error!');
-    }
-    return (
-      <button className={styles.error_btn} onClick={this.showError}>
-        Error!
-      </button>
-    );
-  }
+
+  return (
+    <button className={styles.error_btn} onClick={() => setShowErrorInfo(true)}>
+      Error!
+    </button>
+  );
 }
 
 export default ShowErrorInfo;
